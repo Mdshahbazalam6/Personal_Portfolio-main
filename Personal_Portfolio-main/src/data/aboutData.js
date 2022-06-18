17b69f6124cb5673fa68721fee79a0d4e75acd81
Mdshahbazalam6 <shahbazalam2002620@gmail.com>
2022-06-18 22:02:29 +0530

    ok

--- Personal_Portfolio-main/src/data/aboutData.js
+++ Personal_Portfolio-main/src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Md Shahbaz Alam. I'm a web developer and designer based in Koderma,jharkhand, India",
+    description1: "My name's Md Shahbaz Alam. I'm a web developer and designer based in Koderma, jharkhand, India",
     description2: "⚡ I enjoy developing websites and a keen interest in solving problems and coming up with effective solutions ",
     description3: "⚡ Enjoy building responsive websites using ReactJs, material ui, MongoDB, Nodejs etc",
     image: 2
